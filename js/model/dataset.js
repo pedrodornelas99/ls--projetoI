2e8518e17abb40fd5c1d7eb707c0b5ffc6f38982
Pedro Dornelas <88899934+pedrodornelas99@users.noreply.github.com>
2021-12-01 17:15:51 -0300

    Add files via upload

---
+++ js/model/dataset.js
@@ -0,0 +1,28 @@
+const dataset = [
+  {
+    id: 1,
+    name: 'almoço',
+    image: 'img/foods/1.jpg',
+    price: "R$ 35,00",
+  },
+  {
+    id: 2,
+    name: 'jantar',
+    image: 'imgs/foods/2.jpg',
+    price: "R$ 40,00",
+  },
+  {
+    id: 3,
+    name: 'bebida',
+    image: 'imgs/foods/3.jpg',
+    price: "R$ 15,00",
+  },
+  {
+    id: 4,
+    name: 'sobremesa',
+    image: 'imgs/foods/4.jpg',
+    price: "R$ 20,00",
+  },
+];
+
+export default dataset;
